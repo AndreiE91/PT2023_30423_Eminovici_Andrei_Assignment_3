6f40e52261f66dbc6a8a926174628aae64af2a8d
unknown <eminoviciandrei777@yahoo.com>
2024-02-06 13:51:12 +0200

    Cleaned project structure

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -18,7 +18,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DBURL = "jdbc:mysql://localhost:3306/warehousedb";
     private static final String USER = "root";
-    private static final String PASS = "";
+    private static final String PASS = "19091977Ge#%";
 
     private static ConnectionFactory singleInstance = new ConnectionFactory();
 
